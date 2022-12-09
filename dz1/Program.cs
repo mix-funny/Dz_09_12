2cba658133066c28ebb59a6c922d446a593dc2cb
Michael_SVAO <vierumihail7@gmail.com>
2022-12-09 14:26:19 +0300

    Задача №29

---
+++ dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+int[] x = new int[8];
+int[] GetNewArr(int[] a)
+{
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine("Введите число: ");
+    a[i] = int.Parse(Console.ReadLine()!);
+}
+return a;
+}
+void PrintR(int[] arr)
+{
+    for(int i = 0; i < 8; i++)
+    {
+        Console.Write(arr[i] + " ");
+
+    }
+}
+PrintR(GetNewArr(x));
