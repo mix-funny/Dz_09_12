53b43e9c4b9632b12042578664fc28d64cbd0769
Michael_SVAO <vierumihail7@gmail.com>
2022-12-12 12:37:54 +0300

    Задача №27

---
+++ dz3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма чисел = {GetSum(a)}");
+
+
+int GetSum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+            num = num / 10;
+    }
+    return sum;
+}
