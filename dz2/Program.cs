aa02a22cbbd001ef27a7db4a94a3643a6f48843f
Michael_SVAO <vierumihail7@gmail.com>
2022-12-12 11:39:26 +0300

    Задача № 25

---
+++ dz2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число а: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int Pow(int a, int b)
+{
+    int num = 1;
+    int result = 1;
+    for(num = 1; num <= b; num++ )
+    {
+        result = result * a;
+    }
+    return result;
+}
+int result = Pow(a, b);
+Console.WriteLine($"Число {a} возведенное в степень {b} = {result}");
